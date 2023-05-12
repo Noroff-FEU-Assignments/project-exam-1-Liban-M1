ab30c21d5d4e9877c0541430b293b178856b7329
Liban-M1 <114482781+Liban-M1@users.noreply.github.com>
2023-05-12 13:52:17 +0200

    .

---
+++ js/keychain.js
@@ -0,0 +1,5 @@
+const WP_API_BASE_URL = "https://cdr2b.pro/wp-json/";
+const WP_API_USERNAME = "a1.libanmohammed@gmail.com";
+const WP_API_PASSWORD = "DkJD sOnB opVy ghMb KwXs mvmh";
+
+export { WP_API_BASE_URL, WP_API_USERNAME, WP_API_PASSWORD };
