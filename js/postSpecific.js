240b509c139313ac1a24d4e86cf835b1465119ae
Liban-M1 <114482781+Liban-M1@users.noreply.github.com>
2023-05-28 03:15:36 +0200

    .

---
+++ js/postSpecific.js
@@ -0,0 +1,41 @@
+
+
+
+const postId = new URLSearchParams(window.location.search).get('id');
+const postContent = document.getElementById("post-content");
+const featuredImageContainer = document.getElementById("featured-image");
+const postDetailsContainer = document.getElementById("post-details");
+
+function fetchPost() {
+    const url = `https://cdr2b.pro/wp-json/wp/v2/posts/${postId}`;
+
+    fetch(url)
+    .then((Response) => Response.json())
+    .then(post => {
+        const title = document.createElement("h1");
+        title.textContent = post.title.rendered;
+
+        if (post.featured_media) {
+            const mediaUrl = `https://cdr2b.pro/wp-json/wp/v2/media/${post.featured_media}`;
+            fetch(mediaUrl)
+            .then(Response => Response.json())
+            .then(media => {
+                const image = document.createElement("img");
+                image.src = media.source_url;
+                image.alt = media.alt_text;
+                featuredImageContainer.appendChild(image);
+            })
+            .catch((error) => console.log(error));
+        }
+
+        const content = document.createElement("div");
+        content.innerHTML = post.content.rendered;
+
+        postDetailsContainer.appendChild(title);
+        postDetailsContainer.appendChild(content);
+    })
+    .catch((error) => console.log(error));
+
+}
+
+fetchPost();
